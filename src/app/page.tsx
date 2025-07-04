5b7c5284c2c47ebdb1ef91a660f376b54c783d14
rdin111 <168043983+rdin111@users.noreply.github.com>
2025-07-04 02:05:21 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
               <Certificates />
           </section>
           <section className="container mx-auto mt-20 text-center">
-              <h2 className="text-3xl font-bold mb-8">Featured Projects</h2>
+              <h2 className="text-3xl font-bold mb-6">Featured Projects</h2>
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 text-left">
                   {featuredProjects.map((project) => (
                       <ProjectCard key={project.slug} project={project} />
